1ba55af33eb11015c7fc3918189404ceaf20d323
Denis Makarov <limitofzero@gmail.com>
2016-03-06 19:09:42 +0300

    Add new class MsgFabric

--- TCPChatServer/MsgFabric.h
+++ TCPChatServer/MsgFabric.h
@@ -1,11 +1,14 @@
 #pragma once
 
 #include <string>
+#include <json\json.h>
 
 namespace JsonParser
 {
 	class JValueFabric
 	{
-		
+	public:
+		//создать Json::Value объект
+		virtual Json::Value CreateJsValue() = 0;
 	};
 }
