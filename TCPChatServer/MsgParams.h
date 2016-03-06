1842ad1c9019260570b07ef7e7f2dec1eb472568
Denis Makarov <limitofzero@gmail.com>
2016-03-06 16:50:20 +0300

    Added new enums into MsgParams.h

---
+++ TCPChatServer/MsgParams.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+
+namespace ChatServer
+{
+	//перечисления типов сообщений пользователям
+	enum class MSG_TYPE : uint8_t
+	{
+		MESSAGE = 0,//отпавить сообщение от пользователя
+		ACCEPT,//подключение к серверу и запрос на логина и пароля
+		AUTHORIZE, //удачная авторизация
+		DISC_GUID,//отключение пользователя
+		CON_GUID,//подключение пользователя
+		DISCONNECT//отключение данного пользователя по таймеру
+	};
+
+	//перечисления типов ошибок на сервере(список будет расширяться)
+	enum class SERVER_ERROR : uint8_t
+	{
+		PARSING = 200, //ошибка парсинга
+		AUTHORIZE //ошибка авторизации(неверный логин или пароль)
+	};
+
+
+	//ошибка и их описание
+	const std::unordered_map<SERVER_ERROR, std::string> errorsDescription
+	{
+		emplace(SERVER_ERROR::PARSING, "Ошибка парсинга"),
+		emplace(SERVER_ERROR::AUTHORIZE, "Ошибка авторизации")
+	};
+}
