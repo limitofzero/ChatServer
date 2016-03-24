8aced5346fcd5b82e8520e4ca07cca07cde9830a
Denis Makarov <limitofzero@gmail.com>
2016-03-24 09:13:41 +0300

    Added settings class.

--- TCPChatServer/Settings.h
+++ TCPChatServer/Settings.h
@@ -2,18 +2,23 @@
 
 #include <string>
 #include <memory>
+#include <unordered_map>
 
 namespace ChatServer
 {
-	//структура для загрузги и хранения настроек
-	struct Settings
+	//задел на будущее(пока не трогать)
+	//класс для хранения настроек(паттерн singleton)
+	class Settings
 	{
-		//считывает настройки из ini-файла
-		void Initialize(const std::string &path);
+	public:
+		static Settings &Instance(const std::string &pathToConfig);
 
-		//схемы для валидации
-		std::string authotiringScheme;
-		std::string messageScheme;
+	protected:
+
+
+	private:
+		//схемы для валидации(ключ - наименование схемы)
+		std::unordered_map<std::string, std::string> shemes;
 
 		uint16_t secondsToDisconnect;//время до отключения клиента от сервера
 	};
